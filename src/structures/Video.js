56214d8ad5eb3b8f908ab941e7712fd3c39ef1d4
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-17 15:53:51 -0400

    Fix Video constructor with PlaylistItem

--- src/structures/Video.js
+++ src/structures/Video.js
@@ -5,16 +5,19 @@ class Video {
         this.youtube = youtube;
         this.title = data.snippet.title;
         this.id = data.id.videoId ? data.id.videoId : data.id;
-        if(data.contentDetails) this.id = data.contentDetails.videoId || this.id;
         this.description = data.snippet.description;
-        this.duration = data.contentDetails ? duration.parse(data.contentDetails.duration) : null;
-        this.duration_seconds = this.duration ? duration.toSeconds(this.duration) : -1;
         this.url = `https://www.youtube.com/watch?v=${this.id}`;
         this.publishedAt = data.snippet.publishedAt;
         this.channel = {
             title: data.snippet.channelTitle,
             id: data.snippet.channelId
         };
+
+        if(data.contentDetails) {
+            this.id = data.contentDetails.videoId || this.id;
+            this.duration = data.contentDetails.duration ? duration.parse(data.contentDetails.duration) : null;
+            this.duration_seconds = this.duration ? duration.toSeconds(this.duration) : -1;
+        }
     }
 }
 
