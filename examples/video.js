83c7d2bf022205b6fc10b06e28933d27cf368a0f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-17 19:26:52 -0400

    Update examples

--- examples/video.js
+++ examples/video.js
@@ -2,7 +2,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube('  Y  o  u  r     A  p  i     K  e  y  ');
 
 youtube.videoById('https://www.youtube.com/watch?v=3odIdmuFfEY')
-    .then(results => {
-        console.log(`The video's title is ${results[0].title}`);
+    .then(video => {
+        console.log(`The video's title is ${video[0].title}`);
     })
     .catch(console.log);
