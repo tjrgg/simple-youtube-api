3b2e6e011c6585f0b5275d32a3753f7bb2c3777d
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-17 15:47:12 -0400

    Add YouTube.videoByUrl and Video.duration/duration_seconds

--- src/structures/Video.js
+++ src/structures/Video.js
@@ -1,9 +1,14 @@
+const duration = require('iso8601-duration');
+
 class Video {
     constructor(youtube, data) {
         this.youtube = youtube;
         this.title = data.snippet.title;
-        this.id = data.contentDetails ? data.contentDetails.videoId : data.id.videoId ? data.id.videoId : data.id;
+        this.id = data.id.videoId ? data.id.videoId : data.id;
+        if(data.contentDetails) this.id = data.contentDetails.videoId || this.id;
         this.description = data.snippet.description;
+        this.duration = data.contentDetails ? duration.parse(data.contentDetails.duration) : null;
+        this.duration_seconds = this.duration ? duration.toSeconds(this.duration) : -1;
         this.url = `https://www.youtube.com/watch?v=${this.id}`;
         this.publishedAt = data.snippet.publishedAt;
         this.channel = {
