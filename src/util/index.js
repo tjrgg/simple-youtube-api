d20c2a657d55a6e085d53fa75e93a0448ef5d287
Will Nelson <nelson.will@live.com>
2017-11-19 12:20:50 -0800

    remove useless null parseurl props

--- src/util/index.js
+++ src/util/index.js
@@ -1,13 +1,9 @@
 const { parse } = require('url');
 
-function formatOutput(obj = {}) {
-    return Object.assign({ playlist: null, video: null, channel: null }, obj);
-}
-
 /**
  * Parse a string as a potential YouTube resource URL.
  * @param {string} url
- * @returns {{video, channel, playlist}}
+ * @returns {{video: ?string, channel: ?string, playlist: ?string}}
  */
 exports.parseURL = (url) => {
     const parsed = parse(url, true);
@@ -17,25 +13,25 @@ exports.parseURL = (url) => {
         case 'm.youtube.com': {
             const idRegex = /^[a-zA-Z0-9-_]+$/;
             if (parsed.pathname === '/watch') {
-                if (!idRegex.test(parsed.query.v)) return formatOutput();
-                return formatOutput({
+                if (!idRegex.test(parsed.query.v)) return {};
+                return {
                     video: parsed.query.v,
                     playlist: parsed.query.list || null,
-                });
+                };
             } else if (parsed.pathname === '/playlist') {
-                if(!idRegex.test(parsed.query.list)) return formatOutput();
-                return formatOutput({ playlist: parsed.query.list });
+                if(!idRegex.test(parsed.query.list)) return {};
+                return { playlist: parsed.query.list };
             } else if (parsed.pathname.startsWith('/channel/')) {
                 const id = parsed.pathname.replace('/channel/', '');
-                if (!idRegex.test(id)) return formatOutput();
-                return formatOutput({ channel: id });
+                if (!idRegex.test(id)) return {};
+                return { channel: id };
             }
 
-            return formatOutput();
+            return {};
         }
         case 'youtu.be':
-            return formatOutput({ video: /^\/[a-zA-Z0-9-_]+$/.test(parsed.pathname) ? parsed.pathname.slice(1) : null });
+            return { video: /^\/[a-zA-Z0-9-_]+$/.test(parsed.pathname) ? parsed.pathname.slice(1) : null };
         default:
-            return formatOutput();
+            return {};
     }
 };
