7040d534e1d5be0f4a7f6cd2eb6e96ab7e0090a7
Milan Kragujević <milankragujevic@gmail.com>
2018-02-24 18:57:08 +0100

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -2,7 +2,7 @@ exports.PARTS = {
     Search: 'snippet',
     Videos: 'snippet,contentDetails',
     Playlists: 'snippet',
-    PlaylistItems: 'snippet,contentDetails,status',
+    PlaylistItems: 'snippet,status',
     Channels: 'snippet'
 };
 
