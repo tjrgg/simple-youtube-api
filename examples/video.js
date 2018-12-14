d63cfb5d3c3d4d8a636c46ccdc9b9c294eedf537
Ethan Bellora <Belloramail@gmail.com>
2018-12-14 12:41:55 -0500

    update video.js example
    Example returns object, not an array. The function used is deprecated

--- examples/video.js
+++ examples/video.js
@@ -1,8 +1,8 @@
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube('  Y  o  u  r     A  p  i     K  e  y  ');
 
-youtube.videoById('https://www.youtube.com/watch?v=3odIdmuFfEY')
+youtube.getVideo('https://www.youtube.com/watch?v=3odIdmuFfEY')
     .then(video => {
-        console.log(`The video's title is ${video[0].title}`);
+        console.log(`The video's title is ${video.title}`);
     })
     .catch(console.log);
