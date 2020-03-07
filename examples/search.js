ee3af30eda503c2e1ac53b19325f32b6d99cbe1c
Tyler <rocket0191@gmail.com>
2020-03-07 17:05:02 -0600

    chore: Remove *.js, jsdoc config, travis config

--- examples/search.js
+++
@@ -1,8 +0,0 @@
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('  Y  o  u  r     A  p  i     K  e  y  ');
-
-youtube.searchVideos('Centuries', 4)
-    .then(results => {
-        console.log(`The video's title is ${results[0].title}`);
-    })
-    .catch(console.log);
